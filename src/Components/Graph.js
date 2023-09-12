dcb78be336dddde3e5d4141d5415410680f17e15
Shubham Rana <shubhamrana132@gmail.com>
2023-09-12 13:40:47 +0530

    Added comments few more changes in Graph.js for utility

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -3,9 +3,23 @@ export const START_NODE_Y = 5;
 export const FINISH_NODE_X = 15;
 export const FINISH_NODE_Y = 30;
 
-/* Since the size of each grid box is 44px = 4px of border and 40px of box */
-export const graph_row = parseInt(window.innerHeight / 44) - 1;
-export const graph_col = parseInt(window.innerWidth / 44);
+/*
+  Border is actually 1px which merge with neighbour to form 2px
+  And considering both sides, total of 4px
+*/
+const BORDER_WIDTH = 4
+
+/* A single box in grid is (40px * 40px) in size */
+const GRID_BOX_SIZE = 40
+
+/* Total size of grid box is 40px + 4px */
+const GRID_BOX = GRID_BOX_SIZE + BORDER_WIDTH
+const NAVBAR_HEIGHT_PX = 105.5;
+const NAVBAR_HEIGHT = parseInt(NAVBAR_HEIGHT_PX / GRID_BOX);
+
+/* Since the size of each grid box is 40px = 4px of border and 40px of box */
+export const graph_row = parseInt(window.innerHeight / GRID_BOX) - NAVBAR_HEIGHT;
+export const graph_col = parseInt(window.innerWidth / GRID_BOX);
 
 /* Node for the graph with row index and column index */
 export const Node = (rowId, colId) => {
@@ -21,8 +35,8 @@ export const floatingNode = (rowId, colId) => {
         rowId,
         colId,
         /*
-            x and y are pixels to render the start and finish node
-            44px is the size of each grid box and 9px to center on the grid box 
+          x and y are pixels to render the start and finish node
+          44px is the size of each grid box and 9px to center on the grid box
         */
         x: rowId * 44 + 9,
         y: colId * 44 + 9,
@@ -30,11 +44,11 @@ export const floatingNode = (rowId, colId) => {
 }
 
 /*
-    This is the actual graph node with row index, column index,
-    parent_node -> pointing to its parent in shortest path
-    distance -> distance from source node, default is Infinity 
-    is_visited_node -> boolean to denote if this node was visited during search
-    is_path_node -> boolean to denote if this node is part of shortest path
+  This is the actual graph node with row index, column index,
+  parent_node -> pointing to its parent in shortest path
+  distance -> distance from source node, default is Infinity
+  is_visited_node -> boolean to denote if this node was visited during search
+  is_path_node -> boolean to denote if this node is part of shortest path
 */
 export const GraphNode = (rowId, colId) => {
     return {
