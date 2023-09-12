fcc985156bebe7e083893cd205821646bf27a9ee
Shubham Rana <shubhamrana132@gmail.com>
2023-09-13 00:49:07 +0530

    Added source and destination node in the grid canvas

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,13 @@ const App = () => {
     return (
         <div>
             <Navbar />
+            {/*
+                Hard-coded source to be at location: row = 4 and col = 6
+                Hard-coded destination to be at location: row = 15, col = 20
+                Screen size should be greater than these location as of now
+            */}
+            <div className='source'></div>
+            <div className='destination'></div>
             <table className='table-style'>
                 <tbody>{grid}</tbody>
             </table>
